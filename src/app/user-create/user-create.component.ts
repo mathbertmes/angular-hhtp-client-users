28b3e6c1065e1e4de12f3d8300dc16a0d4b01c7b
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-14 20:27:50 -0700

    feat: user create component working

--- src/app/user-create/user-create.component.ts
+++ src/app/user-create/user-create.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RestApiService } from '../rest-api.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-user-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-create.component.html',
   styleUrl: './user-create.component.css'
 })
 export class UserCreateComponent {
-
+  @Input() userDetails = { name: '', email: '', password: '' };
+    
+    constructor(public restApi: RestApiService,
+        public router: Router) {}
+    ngOnInit() {}
+    addUser(dataUser: any) {
+        this.restApi.createUser(this.userDetails)
+            .subscribe((data: {}) => {
+                this.router.navigate(['/user-list']);
+            });
+    }
 }
