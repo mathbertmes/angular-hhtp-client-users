5c4bdc70c65c052e94fe5a7869bec1ced104d3f5
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-14 18:56:29 -0700

    project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UserCreateComponent } from
+    './user-create/user-create.component';
+import { UserEditComponent } from
+    './user-edit/user-edit.component';
+import { UserListComponent } from
+    './user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'user-create' },
+  { path: 'user-create', component: UserCreateComponent },
+  { path: 'user-list', component: UserListComponent },
+  { path: 'user-edit/:id', component: UserEditComponent },
+];
