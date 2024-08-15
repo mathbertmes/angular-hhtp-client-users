107fa8e2a1d44c84ae428a736dae25792fd91a52
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-14 20:59:00 -0700

    feat: user edit

--- src/app/user-edit/user-edit.component.ts
+++ src/app/user-edit/user-edit.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { RestApiService } from "../rest-api.service";
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-edit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-edit.component.html',
   styleUrl: './user-edit.component.css'
 })
 export class UserEditComponent {
+  userData: any = {};
+  id: string = "";
 
+  constructor(
+    public restApi: RestApiService,
+    public actRoute: ActivatedRoute,
+    public router: Router
+) {}
+
+  ngOnInit() {
+    this.id = this.actRoute.snapshot.params['id'];
+
+    this.restApi.getUser(this.id)
+        .subscribe((data: {}) => {
+            this.userData = data;
+        });
+    
+  }
+
+  updateUser() {
+    if (window.confirm('Are you sure, you want to update?')) {
+        this.restApi.updateUser(this.id, this.userData)
+            .subscribe(data => {
+                this.router.navigate(['/user-list'])
+            });
+    }
+  }
 }
